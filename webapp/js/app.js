39f1b46534d6a6f97947b4800c76eae8adf84ac2
freddadeau <frederic.dadeau@femto-st.fr>
2019-11-16 10:20:14 +0100

    scan bar code OK

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -1,3 +1,5 @@
+"use_strict";
+
 /***
  *      Supermarket scanner application.
  */
@@ -7,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function(_e) {
     /** User info management */
     if (localStorage.getItem("infos")) {
         let obj = JSON.parse(localStorage.getItem("infos"));
+        console.log(obj);
         document.getElementById("numCarte").value = obj.carte;
         document.getElementById("emailUser").value = obj.email;
     }
@@ -22,6 +25,9 @@ document.addEventListener("DOMContentLoaded", function(_e) {
             this.style.display = "none";
             let nextPage = localStorage.getItem("infos") != null ? "bcMain" : "bcInfos";
             document.getElementById(nextPage).style.display = "block";
+        }
+        else {
+            document.querySelector("#bcStart p").innerHTML = "Allez, un petit effort..."   
         }
     }, { passive: true });
     
@@ -42,17 +48,21 @@ document.addEventListener("DOMContentLoaded", function(_e) {
     });
     
     /** Events related to the bcMain block **/
-    document.getElementById("btnAjouter").addEventListener("click", function() {
-        document.getElementById("bcScan").style.display = "block"; 
+    document.getElementById("btnInfos").addEventListener("click", function() {
+            document.getElementById("bcInfos").style.display = "block";
+            document.getElementById("bcMain").style.display = "none";
     });
     
     
+    
+    /**********************************************************************
+     *                          Barcode reader                            *
+     *  Taken from: https://github.com/EddieLa/JOB                        *
+     **********************************************************************/
     JOB.Init();
     JOB.SetImageCallback(function(result) {
-        console.log("result: ", result);
         if (result.length > 0 && result[0].Format == "EAN-13") {
-            document.getElementById("codeEAN").value = result[0].Value;   
-            addProduct();
+            addProduct(result[0].Value);
         }
         else {
             alert("Impossible d'extraire un code-barre");
@@ -60,7 +70,7 @@ document.addEventListener("DOMContentLoaded", function(_e) {
     });
     
     var picture = document.createElement("img");
-    document.getElementById("takePhoto").addEventListener("change", function (event) {
+    document.getElementById("btnAjouter").addEventListener("change", function (event) {
         var files = event.target.files;
         if (files && files.length > 0) {
             var file = files[0];
@@ -91,29 +101,57 @@ document.addEventListener("DOMContentLoaded", function(_e) {
     });
     
     
-    
+    let products = {};
     
     let basket = {
         content: {},
         add: function(p) {
-            if (! content[p]) {
-                content[p] = { quantity: 0, label: products[p].label, price: products[p].price, last: 0 };      
+            if (! products[p]) {
+                return -1;
             }
-            content[p].quantity++;
-            content[p].last = Date.now();
+            if (! this.content[p]) {
+                this.content[p] = { ean: p, quantity: 0, label: products[p].label, price: products[p].price, last: 0 };      
+            }
+            this.content[p].quantity++;
+            this.content[p].last = Date.now();
             return 0;
         },
         display: function() {
-                   
+            let main = document.querySelector("#bcMain main");
+            let header = document.querySelector("#bcMain header");
+            let nb = 0, total = 0;
+            main.innerHTML = "";
+            Object.values(this.content).sort(function(e1, e2) {
+                return e2.last - e1.last;   
+            }).forEach(function(e) {
+                nb += e.quantity;
+                total += e.quantity * e.price;
+                main.innerHTML += "<div data-ean='" + e.ean + 
+                     "' data-quantity='" + e.quantity + 
+                     "' data-price='" + e.price + "'>" +
+                     e.label + "</div>";
+            });
+            header.dataset.total = total;
+            header.dataset.number = nb;
         },
         remove: function(p) {
-             
+            if (! this.content[p]) {
+                return -1;
+            }
+            if (this.content[p].quantity == 1) {
+                delete(this.content[p]);   
+            }
+            else {
+                this.content[p].quantity--;
+                this.content[p].last = Date.now();
+            }
+            return 0;
         }
     };
     
     
-    function addProduct() {
-        let r = basket.add(document.getElementById("codeEAN").value);
+    function addProduct(ean) {
+        let r = basket.add(1*ean);
         if (r == 0) {
             basket.display();
             document.getElementById("bcScan").display = "none";
@@ -123,19 +161,41 @@ document.addEventListener("DOMContentLoaded", function(_e) {
         }   
     }
     
+    function removeProduct(ean) {
+        if (basket.content[ean] && basket.content[ean].quantity == 1) {
+            if (! confirm("Supprimer le produit sélectionné ?")) {
+                return;
+            }
+        }
+        let r = basket.remove(ean);
+        if (r < 0) {
+            // should not happen
+            alert("Produit inconnu");   
+        }
+        else {
+            basket.display();
+        }
+    }
     
-    let products = {};
-//    fetch("./produits.csv").then(function(;
+    
+    /** Loading product list **/
+    /** XMLHttpRequest vs. fetch: fetch comes from ES2015, so old devices may not implement it... */ 
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             readProducts(this.responseText);   
         }
     }
-    
+    xhr.open("GET", "produits.csv");
+    xhr.send();
     
     function readProducts(content) {
-                   
+        content.split("\n").forEach(function(line) {
+            let l = line.split(";").map(function(el) { return el.trim(); });
+            if (l.length == 3) {
+                products[l[0]] = { label: l[1], price: l[2] };   
+            }
+        });
     }
     
 });     
